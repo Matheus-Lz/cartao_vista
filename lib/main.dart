d34438e829004c882f5abc969b94dfbe2743b8dd
matheus02.silva <matheus.privado.pb@gmail.com>
2022-08-17 22:12:00 -0300

    fim da aula

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home:Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
-    )
+  runApp(BusinessCard());
 
-  ));
+  }
+class BusinessCard extends StatelessWidget {
+  const BusinessCard({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home:Scaffold(
+          backgroundColor: Colors.grey,
+          body: SafeArea(
+            child: Container(
+              child:Text("bah", style: TextStyle(fontSize: 20), textAlign: TextAlign.center,.),
+              color:Colors.white,
+              width: 100.0,
+              height: 100.0,
+              margin: EdgeInsets.symmetric(
+                vertical:10.0,
+                horizontal:50.0,
+              )
+
+
+
+            ),
+          ),
+
+        )
+
+    );
+  }
 }
